7abe5a4f60a812a4a15d3ed854ce388eb1243ba2
Gregory Vander Schueren <gregory.vanderschueren@tessares.net>
2019-03-10 18:07:13 +0100

    dissect Convert Header
    Signed-off-by: Gregory Vander Schueren <gregory.vanderschueren@tessares.net>

--- convert.lua
+++ convert.lua
@@ -1,6 +1,10 @@
 convert_protocol = Proto("Convert",  "0-RTT TCP Converter")
 
-convert_protocol.fields = {}
+version      = ProtoField.uint8( "convert.version",      "Version",       base.DEC)
+total_length = ProtoField.uint8( "convert.total_length", "Total Length",  base.DEC)
+unassigned   = ProtoField.uint16("convert.unassigned",   "Unassigned",    base.DEC)
+
+convert_protocol.fields = { version, total_length, unassigned }
 
 function convert_protocol.dissector(buffer, pinfo, tree)
     length = buffer:len()
@@ -9,6 +13,10 @@ function convert_protocol.dissector(buffer, pinfo, tree)
     pinfo.cols.protocol = convert_protocol.name
 
     local subtree = tree:add(convert_protocol, buffer(), "Convert Protocol Data")
+
+    subtree:add(version,        buffer(0,1))
+    subtree:add(total_length,   buffer(1,1))
+    subtree:add(unassigned,     buffer(2,2))
 end
 
 local tcp_port = DissectorTable.get("tcp.port")
