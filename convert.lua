04d9d3fb9b5e8b4e86a402d27ebf4c75b9dc43a8
Gregory Vander Schueren <gregory.vanderschueren@tessares.net>
2019-03-11 14:49:09 +0100

    stop Convert dissection after message
    Assume for the time being that the Convert message fits in a single
    TCP segment.
    
    Signed-off-by: Gregory Vander Schueren <gregory.vanderschueren@tessares.net>

--- convert.lua
+++ convert.lua
@@ -7,14 +7,20 @@ total_length_f = ProtoField.uint8( 'convert.total_length', 'Total length',  base
 unassigned_f   = ProtoField.uint16('convert.unassigned',   'Unassigned',    base.DEC)
 
 tlv_f          = ProtoField.bytes( 'convert.tlv',          'TLV')
-tlv_type_f     = ProtoField.uint8( 'convert.tlv_type',     'TLV Type',      base.DEC)
-tlv_length_f   = ProtoField.uint8( 'convert.tlv_length',   'TLV Length',    base.DEC)
-tlv_value_f    = ProtoField.bytes( 'convert.tlv_value',    'TLV Value')
+tlv_type_f     = ProtoField.uint8( 'convert.tlv_type',     'Type',          base.DEC)
+tlv_length_f   = ProtoField.uint8( 'convert.tlv_length',   'Length',        base.DEC)
+tlv_value_f    = ProtoField.bytes( 'convert.tlv_value',    'Value')
 
 convert_protocol.fields = {
     version_f, total_length_f, unassigned_f,
     tlv_f, tlv_type_f, tlv_length_f, tlv_value_f
 }
+
+tcp_stream_f    = Field.new('tcp.stream')
+ip_src_f        = Field.new('ip.src')
+ip_dst_f        = Field.new('ip.dst')
+tcp_srcport_f   = Field.new('tcp.srcport')
+tcp_dstport_f   = Field.new('tcp.dstport')
 
 function get_tlv_name(tlv_type)
     local tlv_name = 'Unknown TLV type'
@@ -30,12 +36,32 @@ function get_tlv_name(tlv_type)
     return tlv_name
 end
 
+-- For a given TCP connection, we need to remember when we finished parsing the
+-- Convert message in each direction. After that point the dissector is a NOOP.
+-- We thus store the last pkt number that contained Convert protocol data in
+-- each direction for each tcp.stream. For the moment, we assume the first pkt
+-- with Convert data contains the full Convert message.
+convert_end_pkt_num = {}
+
 function convert_protocol.dissector(buffer, pinfo, tree)
+    -- Empty TCP packets do not have Convert data. Ignore.
     local msg_length = buffer:len()
     if msg_length == 0 then return end
 
-    pinfo.cols.protocol = convert_protocol.name
+    local tcp_stream    = tostring(tcp_stream_f())
+    local ip_src        = tostring(ip_src_f())
+    local ip_dst        = tostring(ip_dst_f())
+    local tcp_srcport   = tostring(tcp_srcport_f())
+    local tcp_dstport   = tostring(tcp_dstport_f())
+    local key           = tcp_stream .. ip_src .. ip_dst .. tcp_srcport .. tcp_dstport
 
+    -- We are past the end of the Convert message. Ignore.
+    if convert_end_pkt_num[key] and
+       pinfo.number > convert_end_pkt_num[key] then
+        return
+    end
+
+    pinfo.cols.protocol = convert_protocol.name
     local subtree = tree:add(convert_protocol, buffer(), 'Convert Protocol Data')
     local total_length = buffer(1,1):uint() * 4
 
@@ -59,6 +85,8 @@ function convert_protocol.dissector(buffer, pinfo, tree)
 
         offset = offset + tlv_bytes
     end
+
+    convert_end_pkt_num[key] = pinfo.number
 end
 
 local tcp_port = DissectorTable.get('tcp.port')
